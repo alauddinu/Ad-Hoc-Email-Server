24e8a92f8b09e7829811b28addd81b3efdd60372
Alauddin Ulaganathan <alauddin.ulaganathan@freshworks.com>
2020-04-14 13:54:57 +0530

    removed some blogs

--- client/app/blog/blog.service.ts
+++ client/app/blog/blog.service.ts
@@ -9,12 +9,6 @@ export class BlogService {
 
   blogEntries: Array<BlogEntry> = [
       {
-        date: new Date('Jan 17, 2019').toString(),
-        title: 'How to run AHEM server on docker',
-        imageUrl: '/assets/images/happy-bluish.png',
-        name: 'run-ahem-on-docker'
-      },
-      {
         date: new Date('Nov 13, 2018').toString(),
         title: 'How to setup you own disposable email server',
         imageUrl: '/assets/images/happy-bluish.png',
